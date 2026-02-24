9241c9bf32073575996dd66e64c5570eb100df7b
Thomas Allen <allenhthomas@gmail.com>
2026-02-24 09:58:01 -0700

    Wrote the code for Prep1.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,28 @@
 using System;
 
-class Program
+class MyPrep1Class
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // int x = 10;
+        // x = 10;
+        //! x = "bob";
+
+        // Console.WriteLine("Hello Prep1 World!");
+
+        string firstName;
+        string lastName;
+
+        Console.Write("What is your first name: ");
+        firstName = Console.ReadLine();
+
+        Console.Write("What is your last name: ");
+        lastName = Console.ReadLine();
+        // lastName.ToUpper();
+
+        // Console.WriteLine(lastName, firstName);
+        // Console.WriteLine($"{{{firstName}}} {lastName}");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
     }
 }
