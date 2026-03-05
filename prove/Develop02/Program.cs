32665e34746abb8945c41d27f133fd0e1e64f410
Thomas Allen <allenhthomas@gmail.com>
2026-03-05 09:39:25 -0700

    Working Menu class

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,8 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Menu menu = new Menu();
+        bool done = false;
+        int userResponse;
+        do
+        {
+            userResponse = menu.ProcessMenu();
+            switch (userResponse)
+            {
+                case 1:
+                // TODO Create new Journal Entry
+                // TODO Add entry to list of journal entries
+                break;
+                case 2:
+                // TODO Display Journal Entries
+                break;
+                case 3:
+                // TODO Save journal to file
+                break;
+                case 4:
+                // TODO Load journal from file
+                break;
+                case 5:
+                    done = true;
+                    break;
+            }
+        } while (!done);
 
-        menu.ProcessMenu();
+        
 
 
 
