52d050845ca953009fb95f65a9f9470af78f55ea
Thomas Allen <allenhthomas@gmail.com>
2026-03-05 09:30:38 -0700

    Menu Class working

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Menu menu = new Menu();
+
+        menu.ProcessMenu();
+
+
+
+
+
+
+        // Console.WriteLine("Hello Develop02 World!");
     }
 }
