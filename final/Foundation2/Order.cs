57f67a211543b64ed1bcbab24338941b5d188d8f
Thomas Allen <allenhthomas@gmail.com>
2026-04-01 11:56:03 -0600

    Getting the coding together

---
+++ final/Foundation2/Order.cs
@@ -0,0 +1,47 @@
+
+public class Order
+{
+    private List<Product> _products = new List<Product>();
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double GetTotalCost()
+    {
+        double total = 0;
+
+        foreach (Product p in _products)
+        {
+            total += p.GetTotalCost();
+        }
+
+        total += _customer.LivesInUSA() ? 5 : 35;
+
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        string result = "";
+
+        foreach (Product p in _products)
+        {
+            result += p.GetPackingInfo() + "\n";
+        }
+
+        return result;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"{_customer.GetName()}\n{_customer.GetAddress().GetFullAddress()}";
+    }
+}
